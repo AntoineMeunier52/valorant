d32c1a26371c9567d29bd23c42e2bb6b69d17280
AntoineMeunier52 <meunier.antoine52@gmail.com>
2022-02-04 12:03:01 +0100

    first part of description

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+// let elem = document.querySelector("body");
+
+// elem.addEventListener("wheel", (e) => {
+//   console.log(e);
+// });
+
+// let navbar = document.querySelector(".nav");
+
+// window.onscroll = () => {
+//   console.log(window.scrollY);
+//   if (window.scrollY > 90) {
+//     navbar.classList.add("activ");
+//     console.log("if");
+//   } else {
+//     navbar.classList.remove("activ");
+//     console.log("else");
+//   }
+// };
